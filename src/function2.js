201d0e0a5acebfca7828994665d975d115798f67
ChristWan <christyim21@gmail.com>
2022-05-26 13:39:29 +0800

    添加本地存储

--- src/function2.js
+++ src/function2.js
@@ -5,18 +5,25 @@ const $tabBar = $('#function2 .tab-bar')
 const $tabContent = $('#function2 .tab-content')
 
 $tabBar.on('click', 'li', e => {
-  const $li = $(e.currentTarget)
-  $li
-    .addClass('selected')
-    .siblings()
-    .removeClass('selected')
-  const index = $li.index()
-  $tabContent
-    .children()
-    .eq(index)
-    .addClass('active')
-    .siblings()
-    .removeClass('active')
+    const $li = $(e.currentTarget)
+    $li
+        .addClass('selected')
+        .siblings()
+        .removeClass('selected')
+    const index = $li.index()
+    $tabContent
+        .children()
+        .eq(index)
+        .addClass('active')
+        .siblings()
+        .removeClass('active')
+
+    localStorage.setItem('tabIndex', index)
 })
 
-$tabBar.children().eq(0).trigger('click')
+if (localStorage.getItem('tabIndex') === '1') {
+    $tabBar.children().eq(1).trigger('click')
+} else {
+    $tabBar.children().eq(0).trigger('click')
+}
+
--- src/function3.js
+++ src/function3.js
@@ -2,7 +2,18 @@ import './function3.css'
 import $ from 'jquery'
 
 const $square = $('#function3 #square')
+const localkey = 'function3.active'
+let active = localStorage.getItem(localkey) === 'yes'
+
+$square.toggleClass('active', active)
 
 $square.on('click', () => {
-  $square.toggleClass('active')
+  if ($square.hasClass('active')) {
+    $square.removeClass('active')
+    localStorage.setItem(localkey, 'no')
+  } else {
+    $square.addClass('active')
+    localStorage.setItem(localkey, 'yes')
+  }
 })
+
